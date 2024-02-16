a83bbd988c9077fec161a2dee109973aeefa9de4
hublemon <113337611+hublemon@users.noreply.github.com>
2024-02-17 03:54:52 +0900

    SetTimeForSeoul

--- functions/controller/comments/index.js
+++ functions/controller/comments/index.js
@@ -49,12 +49,14 @@ class CommentController{
     }
     
     formatDateToYYYYMMDD(date) {
-        const year = date.getFullYear();
-        const month = String(date.getMonth() + 1).padStart(2, '0');
-        const day = String(date.getDate()).padStart(2, '0');
-        
+        const options = { timeZone: 'Asia/Seoul' };
+        const year = date.toLocaleString('en-US', { timeZone: 'Asia/Seoul', year: 'numeric' });
+        const month = String(date.toLocaleString('en-US', { timeZone: 'Asia/Seoul', month: '2-digit' })).padStart(2, '0');
+        const day = String(date.toLocaleString('en-US', { timeZone: 'Asia/Seoul', day: '2-digit' })).padStart(2, '0');
+
         return `${year}.${month}.${day}`;
     }
+
 
 
     async putComment(req,res,next){
